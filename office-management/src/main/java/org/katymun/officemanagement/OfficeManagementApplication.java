c7366695892807e486a521c6fc8926dcde3a702b
katymun <munkate2003@gmail.com>
2026-02-04 15:20:17 +0200

    Add project

--- office-management/src/main/java/org/katymun/officemanagement/OfficeManagementApplication.java
+++ office-management/src/main/java/org/katymun/officemanagement/OfficeManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OfficemanagementApplication {
+public class OfficeManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OfficemanagementApplication.class, args);
+        SpringApplication.run(OfficeManagementApplication.class, args);
     }
 
 }
--- office-management/src/test/java/org/katymun/officemanagement/OfficeManagementApplicationTests.java
+++ office-management/src/test/java/org/katymun/officemanagement/OfficeManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OfficemanagementApplicationTests {
+class OfficeManagementApplicationTests {
 
     @Test
     void contextLoads() {
