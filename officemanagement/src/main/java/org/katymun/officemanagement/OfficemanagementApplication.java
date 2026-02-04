e99c737189e64f0b37291a23ca20bd6d5502a85a
katymun <munkate2003@gmail.com>
2026-02-04 15:16:48 +0200

    Initial commit

---
+++ officemanagement/src/main/java/org/katymun/officemanagement/OfficemanagementApplication.java
@@ -0,0 +1,13 @@
+package org.katymun.officemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficemanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficemanagementApplication.class, args);
+    }
+
+}
---
+++ officemanagement/src/test/java/org/katymun/officemanagement/OfficemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.katymun.officemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficemanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
